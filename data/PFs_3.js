4986090221781ade1988fbd7e63585bcbf38d24a
SaraPeskova <34543813+SaraPeskova@users.noreply.github.com>
2018-01-19 17:52:07 +0100

    Add files via upload

---
+++ data/PFs_3.js
@@ -0,0 +1,15 @@
+var json_PFs_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Social Security Trust Fund", "description": "USD 2,847.9 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ -76.6191075, 39.3030641, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Government Pension Investment Fund", "description": "USD 1,280.1 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 139.7490714, 35.6673002, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "National Pension Service", "description": "USD 532.6 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 126.883994, 37.480687, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Federal Retirement Thrift Investment Board", "description": "USD 495.3 billion in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ -77.0063774, 38.9020497, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "ABP Fund", "description": "USD 448.5 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 5.9747732, 50.8818837, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "CPP Investment Board", "description": "USD 350.3 billions in assets in 2017" }, "geometry": { "type": "Point", "coordinates": [ -79.3787042, 43.6522215, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "CalPERS", "description": "USD 302.0 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ -121.5047505, 38.5744139, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Central Provident Fund", "description": "USD 241.4 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 103.8453333, 1.2801239, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "PFZW Fund", "description": "USD 218.0 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 5.21053770000003, 52.0938295, 0.0 ] } }
+]
+}
---
+++ data/SWFs_2.js
@@ -0,0 +1,16 @@
+var json_SWFs_2 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Government Pension Fund Global", "description": "USD 942.8 billions in assets (2016)" }, "geometry": { "type": "Point", "coordinates": [ 10.7423574, 59.9086222, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "China Investment Corporation", "description": "USD 813.5 billions in assets in 2016" }, "geometry": { "type": "Point", "coordinates": [ 116.41687930000001, 39.9096613, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Abu Dhabi Investment Authority", "description": "USD 792.0 billions in assets in 2016 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 54.3513051, 24.4864928, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "SAMA Foreign Holdings", "description": "USD 582.4 billions in assets in 2016 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 46.687407200000102, 24.6640175, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Kuwait Investment Authority", "description": "USD 511.0 billions in assets in 2016 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 47.9756806, 29.3643741, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Hong Kong Monetary Authority", "description": "USD 410.0 billions in assets in 2016 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 114.159468, 22.285295, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "GIC", "description": "USD 359.0 billions in assets in 2017" }, "geometry": { "type": "Point", "coordinates": [ 103.8476427, 1.2778309, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Qatar Investment Authority", "description": "USD 320.0 billions in assets in 2017 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 51.5376431, 25.3230901, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "SAFE Investment Company", "description": "USD  309.0 billions in assets in 2016 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 114.1619302, 22.2812649, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "National Social Security Fund", "description": "USD 288.3 billions in assets in 2015 (estimate)" }, "geometry": { "type": "Point", "coordinates": [ 116.365141, 39.915807, 0.0 ] } }
+]
+}
